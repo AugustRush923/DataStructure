866a16b3466451bc35576278ceef30ca0db0ad56
AugustRush923 <augustrush923@163.com>
2024-04-23 21:27:40 +0800

    feat: update name

--- linked_list/__init__.py
+++ linked_list/__init__.py
@@ -1,5 +1,5 @@
-from linked_list.OneWayLinkedList import OneWayLinkedList
-from linked_list.TwoWayLinkedList import TwoWayLinkedList
+from linked_list.singly import OneWayLinkedList
+from linked_list.doubly import TwoWayLinkedList
 from linked_list.circulate import CirculateLinkedList
 
 singly_linkedList = OneWayLinkedList
